c60c820b675e676db4ca32683d08f2d259cd338b
m1kqsh <muratbekovimran6@gmail.com>
2026-03-29 21:40:48 +0600

    lesson1

--- lesson1.py
+++ lesson1.py
@@ -51,3 +51,49 @@ print(lllidan.greet())
 print(lllidan.attack())
 print(lllidan.rest())
 print(f"После: здоровье = {lllidan.health}, сила = {lllidan.strength}")
+
+# тут уже можно приписать своего перса и его статы
+class Hero:
+    def __init__(self, name, level, health, strength):
+        self.name = name
+        self.level = level
+        self.health = health
+        self.strength = strength
+
+    def greet(self):
+        return f"Привет, я {self.name}, мой уровень {self.level}"
+
+    def attack(self):
+        self.strength -= 1
+        return f"{self.name} наносит удар!"
+
+    def rest(self):
+        self.health += 1
+        return f"{self.name} отдыхает…"
+
+print("----------------------------------")
+
+name1 = input("Имя героя 1: ")
+level1 = int(input("Уровень: "))
+health1 = int(input("Здоровье: "))
+strength1 = int(input("Сила: "))
+
+name2 = input("Имя героя 2: ")
+level2 = int(input("Уровень: "))
+health2 = int(input("Здоровье: "))
+strength2 = int(input("Сила: "))
+
+hero1 = Hero(name1, level1, health1, strength1)
+hero2 = Hero(name2, level2, health2, strength2)
+
+print(hero1.greet())
+print(hero1.attack())
+print(hero1.rest())
+print(f"После: здоровье = {hero1.health}, сила = {hero1.strength}")
+
+print("-----")
+
+print(hero2.greet())
+print(hero2.attack())
+print(hero2.rest())
+print(f"После: здоровье = {hero2.health}, сила = {hero2.strength}")
